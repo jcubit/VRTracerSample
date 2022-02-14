b608a40a73e65d7283f2f32b3135b04303583deb
jcubit <jav.cuesta@gmail.com>
2022-02-14 17:25:41 +0100

    REMOVED: unused types after refactoring

--- VRTracer/ShaderTypes.h
+++ VRTracer/ShaderTypes.h
@@ -7,46 +7,9 @@
 #ifndef ShaderTypes_h
 #define ShaderTypes_h
 
-//#ifdef __METAL_VERSION__
-//#define NS_ENUM(_type, _name) enum _name : _type _name; enum _name : _type
-//#define NSInteger metal::int32_t
-//#else
-//#import <Foundation/Foundation.h>
-//#endif
 
 #include <simd/simd.h>
 
-
-
-//typedef NS_ENUM(NSInteger, BufferIndex)
-//{
-//    BufferIndexMeshPositions = 0,
-//    BufferIndexMeshGenerics  = 1,
-//    BufferIndexUniforms      = 2
-//};
-//
-//typedef NS_ENUM(NSInteger, VertexAttribute)
-//{
-//    VertexAttributePosition  = 0,
-//    VertexAttributeTexcoord  = 1,
-//};
-
-
-//struct Camera {
-//    simd_float3 position;
-//    simd_float3 right;
-//    simd_float3 up;
-//    simd_float3 forward;
-//};
-//
-//
-//struct Uniforms
-//{
-//    unsigned int width;
-//    unsigned int height;
-//    unsigned int frameIndex;
-//    struct Camera camera;
-//};
 
 
 struct FlyCamera {
@@ -65,13 +28,6 @@ struct UniformsFlyCamera
     struct FlyCamera camera;
 };
 
-
-
-
-//typedef NS_ENUM(NSInteger, TextureIndex)
-//{
-//    TextureIndexColor    = 0,
-//};
 
 
 #endif /* ShaderTypes_h */
